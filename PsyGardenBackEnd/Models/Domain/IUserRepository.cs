4b94f54cc9a62e21f910cd8d2e7d1cdd52ea5483
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-04-18 12:12:07 +0200

    Minor change

---
+++ PsyGardenBackEnd/Models/Domain/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsyGardenBackEnd.Models.Domain
+{
+    public interface IUserRepository
+    {
+        User GetByEmail(string email);
+        void Add(User user);
+        void Update(User user);
+        void Delete(User user);
+        void SaveChanges();
+
+    }
+}
