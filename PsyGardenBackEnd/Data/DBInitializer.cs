b37fcdd2faa59befb602825c514c46d8eb542f6b
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-05-08 14:34:21 +0200

    Add description init

--- PsyGardenBackEnd/Data/DBInitializer.cs
+++ PsyGardenBackEnd/Data/DBInitializer.cs
@@ -105,7 +105,34 @@ namespace PsyGardenBackEnd.Data
 
                 #region EVENTS
                 //Event
-                Event psyFi = new Event("Psy-Fi", "Seed of Science", new DateTime(2019, 8, 28, 8, 0, 0),
+
+                #region Descriptions
+                string descriptionPsyFi = "Psy-Fi is an annual open air psychedelic music and arts festival in the Netherlands. The first edition was held" +
+                    " in July 2013 in the Stadspark of the city of Groningen; all subsequent editions have been held in recreation area De Groene Ster to the" +
+                    " east of Leeuwarden. It is one of the largest international festivals in the Netherlands and attracts thousands of visitors each year," +
+                    " with the 2017 edition reporting participants from about 100 different countries.";
+
+                string descriptionOzora = "The Ozora Festival, stylised as O.Z.O.R.A., is an annual psychedelic trance and arts festival in the Hungarian" +
+                    " village of Ozora. The festival has been held on an estate in Ozora near the small vilage of Dádpuszta every year since 2004. The first" +
+                    " modern music festival held in Ozora was called Solipse and took place during the Solar eclipse of August 11, 1999. Solipse did not get " +
+                    " a sequel until the first Ozora Festival was held in 2004. The Ozora Festival is one of the two sizeable psytrance festivals in Hungary (" +
+                    " the other being Solar United Natives festival), and is one of the largest psychedelic trance festivals in the world. As of 2016, it annualy" +
+                    " attracts close to 60,000 people, similar to Boom Festival in Portugal and Antaris Project in Germany who also reach more than 40,000 visitors" +
+                    " every year. Due to its success, several one-day spin-offs from the Ozora Festival have been held in several other countries such as Paris, France" +
+                    " and São Paulo, Brazil.";
+
+                string descriptionSpaceSafari = "Space Safari is an annual open air psychedelic music and arts festival in Belgium.";
+
+                string descriptionBoom = "The Boom Festival is a biennial transformational festival in Portugal. The festival features music performances and a broad" +
+                    " variety of visual art exhibits. Boom festival began in 1997 as a psychedelic trance music festival with a main stage and a lounge area with mellow" +
+                    " 'Chill-Out music'. Now, there are five stages: the Dance Temple, for psytrance and natural trance music; the Alchemy Circle is for many fresh" +
+                    " experimental genres, including psytechno, progressive psytrance, house and unique combinations of those; the Sacred Fire stage is for world music" +
+                    " and live music performances; the Ambient Source is for chill-out music; the Funky Beach stage is for electronic dance music dee jays and producers." +
+                    " Other attractions include sculptures, an art gallery, street theatre, and fire performances. Within the shaded Liminal Village are lectures, yoga classes," +
+                    " film screenings, group meditation, and discussions. The Healing Area has group workshops for healing techniques, a pool for water therapy and a massage area.";
+                #endregion
+
+                Event psyFi = new Event("Psy-Fi", descriptionPsyFi, new DateTime(2019, 8, 28, 8, 0, 0),
                     new DateTime(2019, 9, 1, 23, 0, 0), "Netherlands", null,
                    "Leeuwarden", "De Groene Ster", null, "8926", "https://static1.squarespace.com/static/52e562c3e4b04ab8b0fbe97d/56ef528bd210b8dcd0ac1675/56ef528b22482e26f27c3585/1458524828107/Fractalfest2015_2528.jpg?format=1500w");
                 psyFi.AddEventGenre(psytrance);
@@ -121,7 +148,7 @@ namespace PsyGardenBackEnd.Data
                 psyFi.AddLink(websitePsyFi);
                 psyFi.AddLink(ticketsPsyFi);
 
-                Event ozora = new Event("Ozora Festival", "The Ozora Festival, stylised as O.Z.O.R.A",
+                Event ozora = new Event("Ozora Festival", descriptionOzora,
                     new DateTime(2019, 7, 29, 21, 0, 0),
                     new DateTime(2019, 8, 4, 21, 0, 0), "Hungary", "Ozora", "Dádpuszta", "Dádpuszta", "7015", "7086", "https://i.redd.it/u4zwhkeca6j01.jpg");
                 ozora.AddEventGenre(darkPsytrance);
@@ -132,7 +159,7 @@ namespace PsyGardenBackEnd.Data
                 ozora.AddLink(websiteOzora);
                 ozora.AddLink(ticketsOzora);
 
-                Event spaceSafari = new Event("Space Safari", "Come meet us under the sun!", new DateTime(2019, 8, 30, 14, 0, 0),
+                Event spaceSafari = new Event("Space Safari", descriptionSpaceSafari, new DateTime(2019, 8, 30, 14, 0, 0),
                 new DateTime(2019, 9, 2, 14, 0, 0), "Belgium", "Namen",
                    "Massembre", "Domaine De Massembre", null, "5543", "http://freebornaiden.com/wp-content/uploads/2018/06/11890403_395297293997496_6413516115231520679_o.jpg");
                 spaceSafari.AddEventGenre(psyTech);
@@ -146,7 +173,7 @@ namespace PsyGardenBackEnd.Data
                 spaceSafari.AddLink(websiteSpaceSafari);
                 spaceSafari.AddLink(ticketsSpaceSafari);
 
-                Event boom = new Event("Boom", "Connect with eachother", new DateTime(2020, 7, 28, 0, 0, 0),
+                Event boom = new Event("Boom", descriptionBoom, new DateTime(2020, 7, 28, 0, 0, 0),
                 new DateTime(2020, 8, 4, 0, 0, 0), "Portugal", "Castelo Branco",
                    "Idanha-a-Nova", "Boomland Alcafozes", null, "6060-011", "https://europebookings.com/wp-content/uploads/boom-festival-magic-lights-main-stage.jpg");
                 boom.AddEventGenre(zenonseque);
